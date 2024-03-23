a99a7ee5f1f77c273e4301d95e52d9dabc3f8a9c
netsamma <netsamma@gmail.com>
2024-03-23 15:24:35 +0100

    Os module examples

--- lezione-4/app.js
+++ lezione-4/app.js
@@ -3,23 +3,26 @@
 //**********************************************************************
 
 const os = require('os')
-console.log(os.userInfo())
-console.log("Computer acceso da "+os.uptime()/3600+ " ore" )
+
+// console.log(os.userInfo())
+// console.log("Computer acceso da "+os.uptime()/3600+ " ore" )
 
 
 const info = {
 	so: os.type(),
+	version: os.version(),
 	release: os.release(),
 	memoria: os.totalmem(),
-	disponibile: os.freemem()
+	disponibile: os.freemem(),
+	home: os.homedir(),
 }
 console.log(info)
 
-const path = require('path');
-// Creare un percorso completo a un file
-const nomeFile = 'app.js';
-const percorsoCompleto = path.join(__dirname, nomeFile);
-console.log("Percorso completo al file:", percorsoCompleto);
+// const path = require('path');
+// // Creare un percorso completo a un file
+// const nomeFile = 'app.js';
+// const percorsoCompleto = path.join(__dirname, nomeFile);
+// console.log("Percorso completo al file:", percorsoCompleto);
 
 
 
