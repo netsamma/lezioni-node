70fd7de40119472bcea49a94a791fa2a5c1ac3a0
netsamma <netsamma@gmail.com>
2024-04-02 08:49:38 +0200

    Various edit

--- lezione-5/app.js
+++ lezione-5/app.js
@@ -16,3 +16,66 @@ app.listen(PORT,
 
 // Utilizzo del controller dei prodotti
 app.use('/products', productsController);
+
+
+
+
+
+
+
+
+//**********************************************************************
+// Esercitazione VECCHIA - Server Node con accesso al DB Mysql
+//**********************************************************************
+
+// // File db.js
+
+// const mysql = require('mysql2');
+// // Installare mysql2
+
+// // Configurazione per la connessione al database MySQL
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'ecommerce'
+// });
+
+// // Funzione per ottenere i prodotti dal database
+// function getAllProducts(callback) {
+//   connection.query('SELECT * FROM products', function(err, results) {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, results);
+//     }
+//   });
+// }
+
+// module.exports = {
+//   getAllProducts
+// };
+
+// // File app.js
+
+// const http = require('http');
+// const { getAllProducts } = require('./database'); 
+
+// const PORT = 8080;
+// const HOST = 'http://localhost';
+
+// const server = http.createServer(function (req, res) {
+//   res.setHeader('Content-Type', 'application/json');
+  
+//   // Ottieni i prodotti dal database
+//   getAllProducts(function(err, products) {
+//     if (err) {
+//       res.statusCode = 500;
+//       res.end(JSON.stringify({ error: 'Internal Server Error' }));
+//     } else {
+//       res.end(JSON.stringify(products, null, 2));
+//     }
+//   });
+// }).listen(PORT, () =>
+//   console.log(`Server Running on ${HOST}:${PORT}`)
+// );
